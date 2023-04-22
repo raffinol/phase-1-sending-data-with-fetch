484105d7727718c6f3ef958833a9e672276ef569
Rafael Finol <rafaelfinol88@gmail.com>
2023-04-21 22:31:04 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Add your code here
+function submitData(name, email) {
+  const user = {
+    name,
+    email,
+  };
+
+  const sendData = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+    },
+    body: JSON.stringify(user),
+  };
+  return fetch('http://localhost:3000/users', sendData)
+    .then((response) => response.json())
+    .then(function (object) {
+      document.body.innerHTML = object.id;
+    })
+    .catch(function (error) {
+      document.body.innerHTML = error.message;
+    });
+}
